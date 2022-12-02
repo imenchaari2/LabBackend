ffb69da5be8c9cb21560d13d5c3686ed3d687a7c
imen chaari <imen.chaari@enis.tn>
2022-12-02 16:18:13 +0100

    first commit

--- Article-Service/src/main/java/com/example/articleservice/WebSecurityConfig.java
+++ Article-Service/src/main/java/com/example/articleservice/WebSecurityConfig.java
@@ -1,7 +1,4 @@
-package com.example.msmembre.security;
-import com.example.msmembre.security.jwt.AuthEntryPointJwt;
-import com.example.msmembre.security.jwt.AuthTokenFilter;
-import com.example.msmembre.security.services.UserDetailsServiceImpl;
+package com.example.articleservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,25 +16,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
-  @Autowired
-  UserDetailsServiceImpl userDetailsService;
-
-  @Autowired
-  private AuthEntryPointJwt unauthorizedHandler;
-
-  @Bean
-  public AuthTokenFilter authenticationJwtTokenFilter() {
-    return new AuthTokenFilter();
-  }
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-      authProvider.setUserDetailsService(userDetailsService);
-      authProvider.setPasswordEncoder(passwordEncoder());
-
-      return authProvider;
-  }
+//  @Autowired
+//  UserDetailsServiceImpl userDetailsService;
+//
+//  @Autowired
+//  private AuthEntryPointJwt unauthorizedHandler;
+//
+//  @Bean
+//  public AuthTokenFilter authenticationJwtTokenFilter() {
+//    return new AuthTokenFilter();
+//  }
+//  @Bean
+//  public DaoAuthenticationProvider authenticationProvider() {
+//      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//
+//      authProvider.setUserDetailsService(userDetailsService);
+//      authProvider.setPasswordEncoder(passwordEncoder());
+//
+//      return authProvider;
+//  }
 
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
@@ -52,18 +49,24 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors().and().csrf().disable()
-        .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+        .antMatchers("/api/article/**").permitAll()
         .antMatchers("/api/member/**").permitAll()
         .anyRequest().authenticated();
 
-    http.authenticationProvider(authenticationProvider());
+//    http.authenticationProvider(authenticationProvider());
 
-    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
 
-
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/book-service/books")
+            .permitAll().antMatchers("/eureka/**").hasRole("ADMIN")
+            .anyRequest().authenticated().and().formLogin().and()
+            .logout().permitAll().logoutSuccessUrl("/book-service/books")
+            .permitAll().and().csrf().disable();
+  }
 }
--- Article-Service/src/main/java/com/example/articleservice/proxies/MemberProxy.java
+++ Article-Service/src/main/java/com/example/articleservice/proxies/MemberProxy.java
@@ -3,19 +3,17 @@ package com.example.articleservice.proxies;
 import com.example.articleservice.beans.MemberBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-//@FeignClient(name = "Member-SERVICE" , url = "${member-service.url}")
 @FeignClient(name = "MEMBER-SERVICE")
-//@FeignClient("member-service")
 public interface MemberProxy {
         @GetMapping("/api/member/members")
+        @PreAuthorize("hasRole('ADMIN')")
         List<MemberBean> getAllMembers();
-
         @GetMapping("/api/member/member/{id}")
+        @PreAuthorize("hasRole('ADMIN')")
         MemberBean getMemberById(@PathVariable Long id);
-
-
 }
